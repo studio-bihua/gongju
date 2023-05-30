9135988c6d6062018d43a30b81767495ff5efd3e
Emilien <emilien.ginko@gmail.com>
2023-05-30 11:46:53 +0200

    🔧 export lib

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import  {
+    getRectangle,
+    isVisible,
+    isElement,
+    isTypoReady,
+    scrollTrigger,
+    getTransformProperties,
+    getDocument,
+    getWindow,
+} from "./js/dom" 
+
+import  useOutsideClick from "./react-hooks/useClickOutside" 
+import  useMediaQuery from "./react-hooks/useMediaQuery" 
+import  useMutationObserver from "./react-hooks/useMutationObserver" 
+import  useSearch from "./react-hooks/useSearch" 
+import  useSmoothScroll from "./react-hooks/useSmoothScroll" 
+import  useWindowSize from "./react-hooks/useWindowSize" 
+
+    
+
+export {
+    getWindow, 
+    getDocument, 
+    getTransformProperties, 
+    scrollTrigger,
+    getRectangle,
+    isVisible,
+    isElement,
+    isTypoReady,
+    useOutsideClick,
+    useMediaQuery,
+    useMutationObserver,
+    useSearch,
+    useSmoothScroll,
+    useWindowSize
+}
+
+const gongju = {
+    getRectangle,
+    isVisible,
+    isElement,
+    isTypoReady,
+    scrollTrigger,
+    getTransformProperties,
+    getDocument,
+    getWindow,
+    useOutsideClick,
+    useMediaQuery,
+    useMutationObserver,
+    useSearch,
+    useSmoothScroll,
+    useWindowSize
+}
+
+export default gongju
